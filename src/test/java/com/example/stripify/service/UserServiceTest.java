3ea055e4ab518960c346438d54d47a0cd252e843
Davis Allen <davis.b.allen@gmail.com>
2019-11-02 18:49:15 -0400

    Added UserServiceTest#signup

---
+++ src/test/java/com/example/stripify/service/UserServiceTest.java
@@ -0,0 +1,125 @@
+package com.example.stripify.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+
+import static org.assertj.core.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+import com.example.stripify.model.Song;
+import com.example.stripify.model.User;
+import com.example.stripify.model.UserRole;
+import com.example.stripify.repository.UserRepository;
+import com.example.stripify.util.JwtResponse;
+import com.example.stripify.util.JwtUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.Rule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+//@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+//    @Rule
+//    public MockitoRule mockito = MockitoJUnit.rule();
+
+    private User user1;
+    private Song song1;
+    private UserRole userRole;
+    private List<Song> songs;
+    private String encodedPassword;
+    private String generatedToken;
+    private ObjectMapper objectMapper;
+
+    public UserServiceTest() {
+        user1 = new User();
+        song1 = new Song();
+        userRole = new UserRole();
+        songs = new ArrayList<>();
+        encodedPassword = "asdjkhfjlejhalj3hflkajhf3khakjfhja";
+        generatedToken = "alkjsdlfajlekjlj3lkajflkjaj.flajl3jkldjksfla.lasdjfljelfakje";
+        objectMapper = new ObjectMapper();
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @BeforeEach
+    public void init() {
+        userRole.setId(1);
+        userRole.setName("ROLE_ADMIN");
+
+        songs.add(song1);
+
+        user1.setId(1L);
+        user1.setUsername("user1");
+        user1.setPassword("pw1");
+        user1.setUserRole(userRole);
+        user1.setSongs(songs);
+
+        song1.setId(1L);
+        song1.setTitle("Orphans Unite");
+        song1.setLength(502);
+    }
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    UserRoleService userRoleService;
+
+    @Mock
+    SongService songService;
+
+    @Mock
+    PasswordEncoder bCryptPasswordEncoder;
+
+    @Mock
+    JwtUtil jwtUtil;
+
+    @Test
+    public void signup_ValidUser_Success() throws JsonProcessingException {
+        when(userRoleService.getRole(anyString())).thenReturn(userRole);
+        when(bCryptPasswordEncoder.encode(anyString())).thenReturn(encodedPassword);
+        when(userRepository.save(any())).thenReturn(user1);
+        when(jwtUtil.generateToken(any())).thenReturn(generatedToken);
+        when(userRepository.findByUsername(anyString())).thenReturn(user1);
+
+        JwtResponse jwtResponse = new JwtResponse(generatedToken, user1.getUsername());
+        JwtResponse returnedJwtResponse = userService.signup(user1);
+
+        assertThat(returnedJwtResponse).isNotNull();
+        assertThat(returnedJwtResponse).isEqualToComparingFieldByField(jwtResponse);
+        verify(jwtUtil, times(1)).generateToken(any());
+    }
+
+    @Test
+    public void signup_DuplicateUser_Failure() {
+        user1.setUsername("duplicateUsername");
+        when(userRoleService.getRole(anyString())).thenReturn(userRole);
+        when(bCryptPasswordEncoder.encode(anyString())).thenReturn(encodedPassword);
+        when(userRepository.save(any()))
+                .thenThrow(new ConstraintViolationException("could not execute statement", new SQLException(), "username"));
+
+        Throwable thrown = catchThrowable(() -> userService.signup(user1));
+        assertThat(thrown).isInstanceOf(ConstraintViolationException.class);
+        verify(jwtUtil, never()).generateToken(any());
+    }
+
+
+}
